2ef3ab819455c798a7174b0c1ceaa0e5d7c38180
Sebastian Santiago Alfaro Mendoza <sebas43243@hotmail.com>
2023-08-28 03:27:54 -0500

    finished vocational test mocked

--- src/pages/VocationalTest.js
+++ src/pages/VocationalTest.js
@@ -4,32 +4,42 @@ import { Survey } from "survey-react-ui";
 import 'survey-core/defaultV2.min.css';
 import { surveyJson } from "../components/SurveyJson";
 import { surveyTheme } from "../components/SurveyTheme";
-import { Flex, Heading, Highlight, Stack, Text } from "@chakra-ui/react";
+import { Button, Card, CardBody, CardFooter, CardHeader, Flex, Heading, Highlight, Image, Stack, Text } from "@chakra-ui/react";
+import { useNavigate } from 'react-router-dom';
 
 function VocationalTest() {
   const survey = new Model(surveyJson); // Carga el Json de la encuesta
   survey.applyTheme(surveyTheme); // Aplica el estilo personalizado
 
+  const [showResults, setShowResults] = useState(false); // Estado para mostrar los resultados
+  const [recommendation, setRecommendation] = useState(""); // Estado para almacenar la recomendación
+  const navigate = useNavigate();
+
   survey.onComplete.add((sender) => {
-    handleSurveyOnComplete(sender.data)
+    const answers = sender.data;
+    surveyOnComplete(answers);
+    setShowResults(true);
   });
 
-  const [showAnotherComponent, setShowAnotherComponent] = useState(false);
-
-  const handleSurveyOnComplete = (answers) => {
+  const surveyOnComplete = (answers) => {
     console.log(answers);
     // Envia data al backend
-    setShowAnotherComponent(true);
+    setRecommendation("Ingenieria de sistemas");
+  };
+
+  const handleAcceptButtonClick = () => {
+    navigate("/content");
+    // Logica para enviar la recomendación a la pagina "content"
   };
 
   const SurveyContainer = ({ children }) => ( // Con el propósito de alinear el contenido a la izquierda
     <div style={{ textAlign: "left" }}>{children}</div>
   );
 
-  return (
-    <>
-      <Flex minH={"100vh"} justify={"center"} bg={"purple.100"}>
-        <Stack spacing={4} mx={"auto"} maxW={{ base: "lg", lg: "100%" }} py={12} px={6}>
+  const renderResults = () => {
+    if (!showResults) {
+      return (
+        <>
           <Heading fontSize={"4xl"} textAlign={"center"} color={"purple.700"}>
             Test Vocacional
           </Heading>
@@ -44,8 +54,43 @@ function VocationalTest() {
           <SurveyContainer>
             <Survey model={survey} />
           </SurveyContainer>
+        </>
+      );
+    } else {
+      return (
+        <>
+          <Heading fontSize={"4xl"} color={"purple.700"}>
+            Resultado
+          </Heading>
+          <Card>
+            <CardHeader>
+              <Heading fontSize={["2xl"]} color={'purple.700'}>
+                ¡Felicitaciones!
+              </Heading>
+            </CardHeader>
+            <CardBody>
+              <Image borderRadius='full' src='WomenInStem1.png' mb={5} />
+              <Text fontSize={["md", "lg"]} color={'purple.700'} mb={5}>
+                De acuerdo a sus intereses y preferencias evaluados en el Test Vocacional, podemos recomendarle la siguiente carrera:
+              </Text>
+              <Heading fontSize={"lg"} color={'purple.700'}>1. {recommendation}</Heading> {/* RESPUESTAS DEL BACKEND */}
+            </CardBody>
+            <CardFooter justifyContent={"center"}>
+              <Button onClick={handleAcceptButtonClick} colorScheme="purple">Continuar</Button>
+            </CardFooter>
+          </Card>
+        </>
+      );
+    }
+  };
+
+  return (
+    <>
+      <Flex minH={"100vh"} justify={"center"} bg={"purple.100"}>
+        <Stack spacing={4} mx={"auto"} maxW={{ base: "lg", lg: "100%" }} py={12} px={6}>
+          {renderResults()}
         </Stack>
-      </Flex >
+      </Flex>
     </>
   );
 }
