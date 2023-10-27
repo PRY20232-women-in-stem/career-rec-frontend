072bdf3145c889d4241ac089bf3d94481ba0df56
Sebastian Santiago Alfaro Mendoza <sebas43243@hotmail.com>
2023-10-27 18:31:18 -0500

    changed error page and pre-test y post-test

--- src/util/PostTestJson.js
+++ src/util/PostTestJson.js
@@ -4,46 +4,15 @@ export const postTestJson = {
       "name": "page1",
       "elements": [
         {
-          "type": "comment",
-          "name": "projectParticipation",
-          "title": "Explique en qué consistió su participación en el proyecto. ¿Qué hiciste?",
-          "isRequired": true
-        },
-        {
-          "type": "radiogroup",
-          "name": "projectTimeSpent",
-          "title": "¿Cuánto tiempo le ha dedicado?",
-          "isRequired": true,
-          "choices": [
-            "Entre 0 a 5 minutos",
-            "Entre 10 a 20 minutos",
-            "Entre 25 a 35 minutos",
-            "Entre 40 a 50 minutos",
-            "Entre 55 a 60 minutos"
-          ]
-        }
-      ],
-      "title": "Medida de participación"
-    },
-    {
-      "name": "page2",
-      "elements": [
-        {
           "type": "rating",
           "name": "interestStemFields",
-          "title": "Ahora estoy más interesada en la ciencia, tecnología, ingeniería o matemáticas",
+          "title": "Ahora estoy más interesada en matemáticas, ciencia, tecnología e ingeniería",
           "isRequired": true
         },
         {
           "type": "rating",
           "name": "futureInterestStem",
           "title": "Ahora estoy más interesada que antes en estudiar los campos STEM en el futuro",
-          "isRequired": true
-        },
-        {
-          "type": "rating",
-          "name": "learnNewInfo",
-          "title": "La actividad me ha permitido conocer nueva información sobre estas profesiones",
           "isRequired": true
         },
         {
