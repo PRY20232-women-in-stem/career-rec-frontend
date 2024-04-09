e64569ee029333ae6ca310007a7301737e7cbbe1
Sebastian Santiago Alfaro Mendoza <sebas43243@hotmail.com>
2024-04-09 02:24:17 -0500

    Migrated from CRA to VITE

--- src/config/AxiosAuthConfig.jsx
+++ src/config/AxiosAuthConfig.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: process.env.REACT_APP_URL,
+    baseURL: import.meta.env.VITE_APP_URL,
 });
 
 const getAccessToken = () => {
